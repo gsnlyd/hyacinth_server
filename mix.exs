3f82250d90ba495b5c3395554011f13b00f7a022
gsnlyd <48926197+gsnlyd@users.noreply.github.com>
2022-07-31 15:15:51 -0400

    Correct tailwind version

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule Hyacinth.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:tailwind, "~> 0.1.6", runtime: Mix.env() == :dev},
+      {:tailwind, "~> 0.1.8", runtime: Mix.env() == :dev},
     ]
   end
 
