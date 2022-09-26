ee96d23ec2fbcdddb8f1e691473817d02c242246
gsnlyd <48926197+gsnlyd@users.noreply.github.com>
2022-09-26 16:51:25 -0400

    Add more filter options to datasets page

--- lib/hyacinth_web/live/dataset_live/index.ex
+++ lib/hyacinth_web/live/dataset_live/index.ex
@@ -4,20 +4,23 @@ defmodule HyacinthWeb.DatasetLive.Index do
   alias Hyacinth.Warehouse
   alias Hyacinth.Warehouse.Dataset
 
-  defmodule SortOptions do
+  defmodule FilterOptions do
     use Ecto.Schema
     import Ecto.Changeset
 
+    @primary_key false
     embedded_schema do
+      field :search, :string, default: ""
+      field :type, Ecto.Enum, values: [:all, :root, :derived], default: :all
       field :sort_by, Ecto.Enum, values: [:name, :date_created], default: :date_created
       field :order, Ecto.Enum, values: [:ascending, :descending], default: :descending
     end
 
     @doc false
-    def changeset(sort_options, attrs) do
-      sort_options
-      |> cast(attrs, [:sort_by, :order])
-      |> validate_required([:sort_by, :order])
+    def changeset(filter_options, attrs) do
+      filter_options
+      |> cast(attrs, [:search, :type, :sort_by, :order])
+      |> validate_required([:search, :type, :sort_by, :order])
     end
   end
 
@@ -25,35 +28,35 @@ defmodule HyacinthWeb.DatasetLive.Index do
     socket = assign(socket, %{
       datasets: Warehouse.list_datasets_with_counts(),
 
-      sort_changeset: SortOptions.changeset(%SortOptions{}, %{}),
-      sort_by: :date_created,
-      order: :descending,
+      filter_changeset: FilterOptions.changeset(%FilterOptions{}, %{}),
     })
     {:ok, socket}
   end
 
-  def sort_datasets(datasets, sort_by, order) do
-    sorter =
-      case order do
-        :ascending -> :asc
-        :descending -> :desc
+  def filter_datasets(datasets, filter_changeset) do
+    %FilterOptions{} = options = Ecto.Changeset.apply_changes(filter_changeset)
+
+    datasets_filtered =
+      Enum.filter(datasets, fn {%Dataset{} = dataset, _, _} ->
+        (options.search == "" or String.contains?(String.downcase(dataset.name), String.downcase(options.search))) and
+        (options.type == :all or options.type == dataset.type)
+      end)
+
+    datasets_sorted =
+      case options.sort_by do
+        :name -> Enum.sort_by(datasets_filtered, &(String.downcase(elem(&1, 0).name)))
+        :date_created -> Enum.sort_by(datasets_filtered, &(elem(&1, 0).inserted_at), DateTime)
       end
 
-    Enum.sort_by(datasets, fn {%Dataset{} = dataset, _, _} ->
-      case sort_by do
-        :name -> String.downcase(dataset.name)
-        :date_created -> dataset.inserted_at
-      end
-    end, sorter)
+    case options.order do
+      :ascending -> datasets_sorted
+      :descending -> Enum.reverse(datasets_sorted)
+    end
   end
 
-  def handle_event("sort_updated", %{"sort_options" => params}, socket) do
-    changeset = SortOptions.changeset(%SortOptions{}, params)
-    socket = assign(socket, %{
-      sort_changeset: changeset,
-      sort_by: Ecto.Changeset.get_field(changeset, :sort_by),
-      order: Ecto.Changeset.get_field(changeset, :order),
-    })
+  def handle_event("filter_updated", %{"filter_options" => params}, socket) do
+    changeset = FilterOptions.changeset(%FilterOptions{}, params)
+    socket = assign(socket, :filter_changeset, changeset)
     {:noreply, socket}
   end
 end
