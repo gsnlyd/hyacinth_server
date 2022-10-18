0a6f737a93e99126b83e42cdbd43b7aee793ac3f
gsnlyd <48926197+gsnlyd@users.noreply.github.com>
2022-10-18 00:07:11 -0400

    Add keyboard commands to label interface

--- lib/hyacinth_web/live/label_session_live/label.ex
+++ lib/hyacinth_web/live/label_session_live/label.ex
@@ -32,4 +32,37 @@ defmodule HyacinthWeb.LabelSessionLive.Label do
     })
     {:noreply, socket}
   end
+
+  def handle_event("set_label_key", %{"key" => key}, socket) when key in ["1", "2", "3", "4", "5", "6", "7", "8", "9"] do
+    label_i = String.to_integer(key) - 1
+    label_value = Enum.at(socket.assigns.label_session.job.label_options, label_i)
+    case label_value do
+      label_value when is_binary(label_value) ->
+        Labeling.create_label_entry!(socket.assigns.element, socket.assigns.current_user, label_value)
+
+        labels = Labeling.list_element_labels(socket.assigns.element)
+        socket = assign(socket, %{
+          labels: labels,
+          current_value: hd(labels).label_value,
+        })
+        {:noreply, socket}
+
+      nil ->
+        {:noreply, socket}
+    end
+  end
+
+  def handle_event("set_label_key", _value, socket), do: {:noreply, socket}
+
+  def handle_event("prev_element", _value, socket) do
+    new_index = max(socket.assigns.element.element_index - 1, 0)
+    path = Routes.live_path(socket, HyacinthWeb.LabelSessionLive.Label, socket.assigns.label_session, new_index)
+    {:noreply, push_redirect(socket, to: path)}
+  end
+
+  def handle_event("next_element", _value, socket) do
+    new_index = min(socket.assigns.element.element_index + 1, length(socket.assigns.label_session.elements) - 1)
+    path = Routes.live_path(socket, HyacinthWeb.LabelSessionLive.Label, socket.assigns.label_session, new_index)
+    {:noreply, push_redirect(socket, to: path)}
+  end
 end
