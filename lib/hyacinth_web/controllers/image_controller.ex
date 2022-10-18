f99725996010ed6bcc3012d6eb7ff21e685d1a3f
gsnlyd <48926197+gsnlyd@users.noreply.github.com>
2022-10-17 23:35:46 -0400

    Limit objects which can be served

--- lib/hyacinth_web/controllers/image_controller.ex
+++ lib/hyacinth_web/controllers/image_controller.ex
@@ -2,9 +2,12 @@ defmodule HyacinthWeb.ImageController do
   use HyacinthWeb, :controller
 
   alias Hyacinth.Warehouse
+  alias Hyacinth.Warehouse.Object
 
   def show(conn, %{"object_id" => object_id}) do
-    object = Warehouse.get_object!(object_id)
+    %Object{} = object = Warehouse.get_object!(object_id)
+    if object.type != :blob, do: raise "Only blobs can be served"
+    if object.file_type != :png, do: raise "Only pngs can be served"
 
     path = Warehouse.Store.get_object_path_from_hash(object.hash)
     Phoenix.Controller.send_download(conn, {:file, path}, content_type: "image/png", disposition: :inline)
