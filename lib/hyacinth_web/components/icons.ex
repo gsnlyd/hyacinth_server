f884f8ca57bb63e0a664b9f1914798b6b15b41b7
gsnlyd <48926197+gsnlyd@users.noreply.github.com>
2022-10-18 00:16:40 -0400

    Reuse prev/next events for buttons and add icons

--- lib/hyacinth_web/components/icons.ex
+++ lib/hyacinth_web/components/icons.ex
@@ -37,6 +37,23 @@ defmodule HyacinthWeb.Components.Icons do
   end
 
 
+  def chevron_left_outline(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+    </svg>
+    """
+  end
+
+  def chevron_left_mini(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+      <path fill-rule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clip-rule="evenodd" />
+    </svg>
+    """
+  end
+
+
   def chevron_right_outline(assigns) do
     ~H"""
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
