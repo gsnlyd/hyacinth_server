6c451c1caa6cd99d6a48ff7eb7ab9e7e500eec5d
gsnlyd <48926197+gsnlyd@users.noreply.github.com>
2022-11-16 19:51:58 -0500

    Add tests for PipelineRunLive

---
+++ test/hyacinth_web/live/pipeline_run_live_test.exs
@@ -0,0 +1,31 @@
+defmodule HyacinthWeb.PipelineRunLiveTest do
+  use HyacinthWeb.ConnCase
+
+  import Hyacinth.{WarehouseFixtures, AssemblyFixtures, AccountsFixtures}
+
+  alias Hyacinth.Assembly.PipelineRun
+
+  setup :register_and_log_in_user
+
+  describe "PipelineRunLive.Show" do
+    test "renders pipeline run", %{conn: conn} do
+      pipeline = pipeline_fixture("My Pipeline")
+      dataset = root_dataset_fixture("My Dataset")
+      user = user_fixture(%{email: "someuser@example.com"})
+      %PipelineRun{} = pipeline_run = pipeline_run_fixture(pipeline, dataset, user)
+
+      {:ok, _view, html} = live(conn, Routes.live_path(conn, HyacinthWeb.PipelineRunLive.Show, pipeline_run))
+
+      assert html =~ "My Pipeline"
+      assert html =~ "My Dataset"
+      assert html =~ "someuser@example.com"
+    end
+
+    @tag :disable_login
+    test "fails if user is not logged in", %{conn: conn} do
+      %PipelineRun{} = pipeline_run = pipeline_run_fixture()
+      {:error, {:redirect, redirect_params}} = live(conn, Routes.live_path(conn, HyacinthWeb.PipelineRunLive.Show, pipeline_run))
+      assert %{flash: %{"error" => "You must log in to access this page."}} = redirect_params
+    end
+  end
+end
