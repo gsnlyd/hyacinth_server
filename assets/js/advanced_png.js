c9d1b9e9cb4a7f13141dffe13a1c65c596ba3cf0
gsnlyd <48926197+gsnlyd@users.noreply.github.com>
2022-12-18 17:23:10 -0500

    Add min controls to advanced png viewer

--- assets/js/advanced_png.js
+++ assets/js/advanced_png.js
@@ -34,9 +34,10 @@ function decode(buffer) {
     return image;
 }
 
-function drawGrayscale(imageData, image, maxThreshold) {
+function drawGrayscale(imageData, image, minThreshold, maxThreshold) {
     for (let i = 0; i < image.numPixels; i++) {
-        const value = (Math.min(image.typedData[i], maxThreshold) / maxThreshold) * 255;
+        const clamped = Math.max(Math.min(image.typedData[i], maxThreshold), minThreshold);
+        const value = ((clamped - minThreshold) / (maxThreshold - minThreshold)) * 255;
         const canvasIndex = i * 4;
         imageData.data[canvasIndex + 0] = value; // R
         imageData.data[canvasIndex + 1] = value; // G
@@ -45,24 +46,24 @@ function drawGrayscale(imageData, image, maxThreshold) {
     }
 }
 
-function drawRGBA(imageData, image, maxThreshold) {
+function drawRGBA(imageData, image, minThreshold, maxThreshold) {
     for (let i = 0; i < image.data.length; i++) {
-        const clamped = Math.min(image.data[i], maxThreshold);
-        const mapped = (clamped / maxThreshold) * 255;
-        imageData.data[i] = mapped;
+        const clamped = Math.max(Math.min(image.data[i], maxThreshold), minThreshold);
+        const value = ((clamped - minThreshold) / (maxThreshold - minThreshold)) * 255;
+        imageData.data[i] = value;
     }
 }
 
-function render(canvas, image, maxThreshold) {
+function render(canvas, image, minThreshold, maxThreshold) {
     const context = canvas.getContext('2d');
     const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
 
     switch (image.ctype) {
         case 0:
-            drawGrayscale(imageData, image, maxThreshold);
+            drawGrayscale(imageData, image, minThreshold, maxThreshold);
             break;
         case 6:
-            drawRGBA(imageData, image, maxThreshold);
+            drawRGBA(imageData, image, minThreshold, maxThreshold);
             break;
         default:
             throw new Error(`Unsupported png color type: ${image.ctype}`)
@@ -75,7 +76,13 @@ export function createHook() {
     return {
         mounted() {
             this.image = null;
-            this.maxThreshold = 1000;
+            this.minThreshold = 0;
+            this.maxThreshold = 100;
+
+            this.minSliderEl = document.getElementById('advanced-png-viewer-min-slider');
+            this.minInputEl = document.getElementById('advanced-png-viewer-min-input');
+            this.maxSliderEl = document.getElementById('advanced-png-viewer-max-slider');
+            this.maxInputEl = document.getElementById('advanced-png-viewer-max-input');
 
             const url = '/object-image/' + this.el.dataset.objectId;
             fetch(url)
@@ -87,12 +94,51 @@ export function createHook() {
                     this.el.width = this.image.width;
                     this.el.height = this.image.height;
                 })
-                .then(() => render(this.el, this.image, (this.maxThreshold / 1000) * this.image.maxValue))
+                .then(() => {
+                    this.minSliderEl.min = this.image.minValue;
+                    this.minSliderEl.max = this.image.maxValue;
+                    this.minSliderEl.value = this.image.minValue;
+                    this.minInputEl.min = this.image.minValue;
+                    this.minInputEl.max = this.image.maxValue;
+                    this.minInputEl.value = this.image.minValue;
 
-            document.getElementById('advanced-png-viewer-max-slider').addEventListener('input', ev => {
-                this.maxThreshold = ev.currentTarget.value;
-                if (this.image) render(this.el, this.image, (this.maxThreshold / 1000) * this.image.maxValue);
-            });
+                    this.minThreshold = this.image.minValue;
+
+                    this.maxSliderEl.min = this.image.minValue;
+                    this.maxSliderEl.max = this.image.maxValue;
+                    this.maxSliderEl.value = this.image.maxValue;
+                    this.maxInputEl.min = this.image.minValue;
+                    this.maxInputEl.max = this.image.maxValue;
+                    this.maxInputEl.value = this.image.maxValue;
+
+                    this.maxThreshold = this.image.maxValue;
+
+                    render(this.el, this.image, this.minThreshold, this.maxThreshold);
+                });
+
+            updateMinThreshold = (minThreshold) => {
+                minThreshold = parseInt(minThreshold);
+                if (minThreshold >= this.maxThreshold) minThreshold = this.maxThreshold - 1;
+                this.minThreshold = minThreshold;
+                this.minSliderEl.value = minThreshold;
+                this.minInputEl.value = minThreshold;
+                if (this.image) render(this.el, this.image, this.minThreshold, this.maxThreshold);
+            }
+
+            this.minSliderEl.addEventListener('input', ev => updateMinThreshold(ev.currentTarget.value));
+            this.minInputEl.addEventListener('change', ev => updateMinThreshold(ev.currentTarget.value));
+
+            updateMaxThreshold = (maxThreshold) => {
+                maxThreshold = parseInt(maxThreshold);
+                if (maxThreshold <= this.minThreshold) maxThreshold = this.minThreshold + 1;
+                this.maxThreshold = maxThreshold;
+                this.maxSliderEl.value = maxThreshold;
+                this.maxInputEl.value = maxThreshold;
+                if (this.image) render(this.el, this.image, this.minThreshold, this.maxThreshold);
+            }
+
+            this.maxSliderEl.addEventListener('input', ev => updateMaxThreshold(ev.currentTarget.value));
+            this.maxInputEl.addEventListener('change', ev => updateMaxThreshold(ev.currentTarget.value));
         }
     }
 }
