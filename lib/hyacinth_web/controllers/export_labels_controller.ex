6299e81c492619e563b975e56cf9434b226590f3
gsnlyd <48926197+gsnlyd@users.noreply.github.com>
2023-01-23 13:33:47 -0500

    Add export labels modal and make some columns optional

--- lib/hyacinth_web/controllers/export_labels_controller.ex
+++ lib/hyacinth_web/controllers/export_labels_controller.ex
@@ -5,14 +5,24 @@ defmodule HyacinthWeb.ExportLabelsController do
   alias Hyacinth.Warehouse.Object
   alias Hyacinth.Labeling.{LabelSession, LabelElement, LabelEntry}
 
-  def show(conn, %{"session_id" => session_id}) do
-    csv = build_session_labels_csv(session_id)
+  def show(conn, %{"session_id" => session_id} = params) do
+    args =  [
+      iso_timestamps:   params["timestamp_columns"] in ["iso", "iso_and_unix"],
+      unix_timestamps:  params["timestamp_columns"] in ["unix", "iso_and_unix"],
+      object_names:     params["object_columns"] in ["names", "names_and_hashes"],
+      object_hashes:    params["object_columns"] in ["hashes", "names_and_hashes"],
+    ]
+
+    csv =
+      session_id
+      |> build_session_labels_csv(args)
+      |> rows_to_string()
 
     Phoenix.Controller.send_download(conn, {:binary, csv}, filename: "session_labels.csv", content_type: "text/plain", disposition: :inline)
   end
 
-  @spec build_session_labels_csv(term) :: String.t
-  defp build_session_labels_csv(session_id) do
+  @spec build_session_labels_csv(term, keyword) :: [[String.t]]
+  defp build_session_labels_csv(session_id, args) do
     %LabelSession{} = session = Labeling.get_label_session_with_elements!(session_id)
 
     num_objects = length(hd(session.elements).objects)
@@ -21,8 +31,8 @@ defmodule HyacinthWeb.ExportLabelsController do
       1..num_objects
       |> Enum.map(fn i ->
         [
-          "object_#{i}_name",
-          "object_#{i}_hash",
+          if(args[:object_names], do: "object_#{i}_name"),
+          if(args[:object_hashes], do: "object_#{i}_hash"),
         ]
       end)
       |> Enum.concat()
@@ -30,8 +40,10 @@ defmodule HyacinthWeb.ExportLabelsController do
     header = [
       "element_index",
       "label_option",
-      "started_at_timestamp",
-      "completed_at_timestamp",
+      if(args[:iso_timestamps], do: "started_at_iso"),
+      if(args[:unix_timestamps], do: "started_at_unix"),
+      if(args[:iso_timestamps], do: "completed_at_iso"),
+      if(args[:unix_timestamps], do: "completed_at_unix"),
     ] ++ header_object_cols
 
     body =
@@ -41,8 +53,8 @@ defmodule HyacinthWeb.ExportLabelsController do
           element.objects
           |> Enum.map(fn %Object{} = object ->
             [
-              object.name,
-              object.hash,
+              if(args[:object_names], do: object.name),
+              if(args[:object_hashes], do: object.hash),
             ]
           end)
           |> Enum.concat()
@@ -52,15 +64,26 @@ defmodule HyacinthWeb.ExportLabelsController do
           [
             element.element_index,
             label.value.option,
-            label.metadata.started_at |> DateTime.to_unix() |> Integer.to_string(),
-            label.metadata.completed_at |> DateTime.to_unix() |> Integer.to_string(),
+            if(args[:iso_timestamps], do: label.metadata.started_at |> Calendar.strftime("%c")),
+            if(args[:unix_timestamps], do: label.metadata.started_at |> DateTime.to_unix() |> Integer.to_string()),
+            if(args[:iso_timestamps], do: label.metadata.completed_at |> Calendar.strftime("%c")),
+            if(args[:unix_timestamps], do: label.metadata.completed_at |> DateTime.to_unix() |> Integer.to_string()),
           ] ++ object_cols
         end)
       end)
       |> Enum.concat()
 
     [header] ++ body
-    |> Enum.map(fn row when is_list(row) -> Enum.join(row, ",") end)
+  end
+
+  @spec rows_to_string([[String.t]]) :: String.t
+  defp rows_to_string(rows) when is_list(rows) do
+    rows
+    |> Enum.map(fn row when is_list(row) ->
+      row
+      |> Enum.filter(&(&1 != nil))
+      |> Enum.join(",")
+    end)
     |> Enum.join("\n")
   end
 end
--- lib/hyacinth_web/live/label_session_live/show.ex
+++ lib/hyacinth_web/live/label_session_live/show.ex
@@ -6,6 +6,24 @@ defmodule HyacinthWeb.LabelSessionLive.Show do
   alias Hyacinth.Warehouse.Object
   alias Hyacinth.Labeling.{LabelElement, LabelJobType}
 
+  defmodule ExportLabelsForm do
+    use Ecto.Schema
+    import Ecto.Changeset
+
+    @primary_key false
+    embedded_schema do
+      field :timestamp_columns, Ecto.Enum, values: [:iso, :unix, :iso_and_unix], default: :iso_and_unix
+      field :object_columns, Ecto.Enum, values: [:names, :hashes, :names_and_hashes], default: :names_and_hashes
+    end
+
+    @doc false
+    def changeset(struct, attrs) do
+      struct
+      |> cast(attrs, [:timestamp_columns, :object_columns])
+      |> validate_required([:timestamp_columns, :object_columns])
+    end
+  end
+
   def mount(params, _session, socket) do
     label_session = Labeling.get_label_session_with_elements!(params["label_session_id"])
 
@@ -13,8 +31,41 @@ defmodule HyacinthWeb.LabelSessionLive.Show do
       label_session: label_session,
       num_labeled: Enum.count(label_session.elements, &(length(&1.labels) > 0)),
       num_total: length(label_session.elements),
+
+      modal: nil,
     })
 
     {:ok, socket}
+  end
+
+  def handle_event("open_modal_export_labels", _value, socket) do
+    changeset = ExportLabelsForm.changeset(%ExportLabelsForm{}, %{})
+    {:noreply, assign(socket, :modal, {:export_labels, changeset})}
+  end
+
+  def handle_event("export_labels_change", %{"export_labels_form" => params}, socket) do
+    changeset = ExportLabelsForm.changeset(%ExportLabelsForm{}, params)
+    {:noreply, assign(socket, :modal, {:export_labels, changeset})}
+  end
+
+  def handle_event("export_labels_submit", %{"export_labels_form" => params}, socket) do
+    changeset = ExportLabelsForm.changeset(%ExportLabelsForm{}, params)
+
+    case Ecto.Changeset.apply_action(changeset, :insert) do
+      {:ok, %ExportLabelsForm{} = struct} ->
+        args = [
+          timestamp_columns: struct.timestamp_columns,
+          object_columns: struct.object_columns,
+        ]
+        path = Routes.export_labels_path(socket, :show, socket.assigns.label_session, args)
+        {:noreply, redirect(socket, to: path)}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, :modal, {:export_labels, changeset})}
+    end
+  end
+
+  def handle_event("close_modal", _value, socket)  do
+    {:noreply, assign(socket, :modal, nil)}
   end
 end
